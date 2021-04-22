7b3c38623ba63a8700b421704cafb9249937df9b
Jonas <jonaslagoteixeira@gmail.com>
2021-04-22 20:53:29 -0300

    feito configuracao inicial

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Logo = () => {
+  return (
+    <S.Wrapper>
+      <h1>Logo</h1>
+    </S.Wrapper>
+  );
+};
+
+export default Logo;
